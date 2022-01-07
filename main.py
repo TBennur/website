587bb2595a639fa1823b262d4c9b1708525829a9
TBennur <tanay.bennur@gmail.com>
2022-01-07 10:21:34 -0500

    Moved general page logic to Javascript

--- main.py
+++ main.py
@@ -2,48 +2,20 @@ from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
-stylizer_status = {"Pallet Selected": False, "Current Image": "Beach", "Current Pallet Size": None, "Current Pallet": None}
-
 @app.route("/")
 def home():
     return render_template("home.html")
     
 @app.route("/stylizer")
 def stylizer():
-    stylizer_status["Pallet Selected"] = False
-    stylizer_status["Current Image"] = "Beach"
-    stylizer_status["Current Pallet Size"] = None
-    stylizer_status["Current Pallet"] = None
     return render_template("stylizer.html")
 
-@app.route('/stylize-button')
+@app.route('/stylize-button', methods = ["GET"])
 def stylize_button():
-    print(stylizer_status)
-    return ("nothing")
-
-
-@app.route('/update-stylize-information', methods=["GET"])
-def update_stylize_information():
-    selection_type = request.args.get("selection_type")
-    if selection_type == "Image Selected":
-        image_selection = request.args.get("image_selection")
-        stylizer_status["Current Image"] = image_selection
-    elif selection_type == "Deselect Pallet Size":
-        stylizer_status["Pallet Selected"] = False
-        stylizer_status["Current Pallet Size"] = None
-        stylizer_status["Current Pallet"] = None
-    elif selection_type == "Select Pallet Size":
-        pallet_size = request.args.get("pallet_size")
-        stylizer_status["Current Pallet Size"] = pallet_size
-    elif selection_type == "Deselect Pallet":
-        stylizer_status["Pallet Selected"] = False
-        stylizer_status["Current Pallet"] = None
-    elif selection_type == "Select Pallet":
-        pallet_name = request.args.get("pallet_name")
-        stylizer_status["Pallet Selected"] = True
-        stylizer_status["Current Pallet"] = pallet_name
-    else:
-        print("Invalid Update Call")
+    print(request.args.get("pallet_selected"))
+    print(request.args.get("current_image"))
+    print(request.args.get("current_pallet_size"))
+    print(request.args.get("current_pallet"))
     return ("nothing")
 
 @app.route("/about")
