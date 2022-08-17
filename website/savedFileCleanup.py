36f9c44220921414dfda00d95f733929c6fe35d2
Tanay Bennur <tanay.bennur@gmail.com>
2022-08-17 17:37:55 +0000

    Fixed Cleanup Pathing

--- website/savedFileCleanup.py
+++ website/savedFileCleanup.py
@@ -1,13 +1,15 @@
 import platform
 import os
+import pathlib
 
-# Import Image Filter Module
-if platform.system() == "Windows":
-    UPLOAD_FOLDER = 'imagefilter/website/temporaryImageFiles'
-    LOGFILE = "imagefilter/website/temporaryImageFileLog.txt"
-else:
-    UPLOAD_FOLDER = 'website/temporaryImageFiles'
-    LOGFILE = "website/temporaryImageFileLog.txt"
+current_path = pathlib.Path("..")
+
+def get_file_path(stem):
+    for path in current_path.rglob(stem):
+        return str(path)
+
+UPLOAD_FOLDER = get_file_path("temporaryImageFiles")
+LOGFILE = get_file_path("temporaryImageFileLog.txt")
 
 MAX_FILE_AGE = 2
 
@@ -17,8 +19,11 @@ with open(LOGFILE,'r',encoding='utf-8') as logfile:
 for i in range(len(data) - 1, -1, -1):
     [filename, file_age] = data[i].split(" ")
     if int(file_age) >= MAX_FILE_AGE:
-        os.remove(os.path.join(UPLOAD_FOLDER, filename))
         data.pop(i)
+        try:
+            os.remove(os.path.join(UPLOAD_FOLDER, filename))
+        except:
+            pass
     else:
         data[i] = filename + " " + str(int(file_age) + 1) + "\n"
 
