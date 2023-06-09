e8f88f15bac6e3b77cd9bd3a4d3aea1ac1eb669a
TBennur <tanay.bennur@gmail.com>
2023-06-09 01:08:24 -0400

    Started Stylizer Jinja Conversion

--- website/app.py
+++ website/app.py
@@ -35,29 +35,44 @@ def home():
     return render_template("home.html")
 
 # Stylizer Route    
-@app.route("/stylizer")
+@app.route("/stylizer", methods = ["GET", "POST"])
 def stylizer():
-    session["id"] = uuid.uuid4()
-    return render_template("stylizer.html")
+    if request.method == 'POST':
+        if "palette-primary" in request.form:
+            session["stylizer"]["primary_palette"] = request.form["palette-primary"]
+            if "palette-secondary" in request.form:
+                session["stylizer"]["secondary_palette"] = request.form["palette-secondary"]
+                if request.form["palette-primary"] != "Please Select a Palette Size" and request.form["palette-secondary"] != "Please Select a Palette":
+                    palette_info = conversion_dictionary[request.form["palette-secondary"]]
+                    img = stylizerWidget.visualize_palette(palette_info["filename"], palette_info["size"])
+                    session["stylizer"]["palette_image"] = "data:image/png;base64," + appUtilities.get_palette(img)
+    else:
+        session["id"] = uuid.uuid4()
+        session["stylizer"] = {"primary_palette": "Please Select a Palette Size",
+                               "secondary_palette": "Please Select a Palette",
+                               "palette_image": "data:image/png;base64,"}
+   
+    context = {"primary_palettes": ["Please Select a Palette Size", "Small", "8-Color", "16-Color", "32-Color", "Large"],
+               "primary_selected": session["stylizer"]["primary_palette"] != "Please Select a Palette Size",
+               "primary_palette": session["stylizer"]["primary_palette"],
+               "secondary_palettes": conversion_dictionary[session["stylizer"]["primary_palette"]],
+               "secondary_selected": session["stylizer"]["primary_palette"] != "Please Select a Palette Size" and session["stylizer"]["secondary_palette"] != "Please Select a Palette",
+               "secondary_palette": session["stylizer"]["secondary_palette"],
+               "palette_image": session["stylizer"]["palette_image"]}
+    
+    return render_template("stylizer.html", **context)
 
 # Main stylization function, creates and converts stylized image
 @app.route('/stylize-button', methods = ["GET", "POST"])
 def stylize_button():
-    image, palette_info, is_custom = appUtilities.get_image_info(request, conversion_dictionary)
+    image, is_custom = appUtilities.get_image_info(request, conversion_dictionary)
+    palette_info = conversion_dictionary[session["stylizer"]["secondary_palette"]]
     try:
         img = stylizerWidget.convert_image(image, palette_info['filename'], palette_info['size'], is_custom, session["id"])    
         encoded_img = appUtilities.get_image(img)
         return jsonify({ 'Status' : 'Success', 'ImageBytes': encoded_img})
     except AttributeError:
         return jsonify({'Status' : 'Failure', 'Reason' : 'You recently uploaded a different file. Due to space constraints, only 1 user file can be processed at a time. Please reupload this file.'})
-
-# Visualizes current palette
-@app.route('/visualize-palette', methods = ["GET", "POST"])
-def visualize_palette():
-    palette_info = appUtilities.get_palette_info(request, conversion_dictionary)
-    img = stylizerWidget.visualize_palette(palette_info['filename'], palette_info['size'])
-    encoded_img = appUtilities.get_palette(img)
-    return jsonify({ 'Status' : 'Success', 'ImageBytes': encoded_img})
 
 # Visualizes Uploaded Image
 @app.route('/visualize-image', methods = ["GET", "POST"])
